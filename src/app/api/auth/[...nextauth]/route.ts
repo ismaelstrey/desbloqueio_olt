0540e7b8da73b9347a825b12daa19d185c7f1220
ismael strey pereira <ismaelstrey@hotmail.com>
2025-04-29 16:04:13 -0300

    feat(auth): add authorization params for GoogleProvider
    Include `prompt`, `access_type`, and `response_type` in Google OAuth configuration to ensure consent and offline access

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,6 +10,13 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
