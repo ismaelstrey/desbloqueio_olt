35d407ed2206f2db6d29335f0adc91ba3483bae7
ismael strey pereira <ismaelstrey@hotmail.com>
2025-05-02 14:46:51 -0300

    style: format indentation and remove unnecessary whitespace
    The changes include fixing indentation and removing redundant blank lines in the PageNovoTicket component to improve code readability and maintain consistency.

--- src/app/os/novo-ticket/page.tsx
+++ src/app/os/novo-ticket/page.tsx
@@ -1,13 +1,8 @@
 import Voltar from "@/app/ui/Voltar";
 import NovoTicket from "@/components/NovoTicket";
-
-
 export default function PageNovoTicket() {
-  
-
-
   return <div>
-       <Voltar/>
-    <NovoTicket/>
+          <Voltar/>
+          <NovoTicket/>
     </div>
 }
